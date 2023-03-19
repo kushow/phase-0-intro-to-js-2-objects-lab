6f7fc46d8ef0865a554912721f4584d6dbcd5da0
mohamednoor <kushow8@gmail.com>
2023-03-20 00:33:17 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+// Initializing employee object
+const employee = {
+    name: "John",
+    streetAddress: "123 Main St"
+  };
+  
+  // Function to update employee object with a key-value pair
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value };
+  }
+  
+  // Function to destructively update employee object with a key-value pair
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  // Function to delete a key-value pair from employee object
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  // Function to destructively delete a key-value pair from employee object
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
